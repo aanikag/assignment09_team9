41a055f40d2681ac82164b63e792d3f1d3d929b4
garreaa <garreaa@vdi-LCB-IC-002.ad.uc.edu>
2024-03-28 13:04:05 -0400

    First commit

---
+++ garreaa_rainfojp_marti6aj_Assignment09/mainPakage/main.py
@@ -0,0 +1,13 @@
+# Name: Aanika Garre, Joseph Rainford, Andrew Martin
+# email: garreaa@mail.uc.edu, rainfojp@mail.uc.edu, marti6aj@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/4/2024
+# Course/Section: IS4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This assignment focuses on calling APIs.
+
+# Brief Description of what this module does: This module focuses on using JSON and calling APIs in Eclipse.
+# Citations:
+# Anything else that's relevant:
+
+#main.py 
